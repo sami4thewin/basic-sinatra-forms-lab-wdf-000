af5546fac5ff8ae6fa06f61aec6a4d58800b7fb0
sami4thewin <samiomarmohammed@gmail.com>
2016-08-30 17:37:16 -0400

    Done.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 ENV["SINATRA_ENV"] = "test"
 require_relative '../config/environment'
 require 'rack/test'
+require 'capybara'
+require 'capybara/dsl'
 require 'capybara/poltergeist'
 
 Capybara.javascript_driver = :poltergeist
